9795bb3f2ebb79eb9b8bb5dc00836ff7e58a7ea2
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-11 23:07:26 -0500

    Add a reason to deny

--- commands/deny.js
+++ commands/deny.js
@@ -18,12 +18,26 @@ exports.run = async (client, message, args) => {
   embed.addField('Client User Name', botuser.user.username, true);
   embed.addField('Client User Mention', `<@!${thisUser}>`, true);
   embed.addField('Client User ID', thisUser, true);
+  try {
+    embed.addField('Reason', `${args.splice(2).join(' ')}`, true);
+  } catch (err) {
+    embed.addField('Reason', err, true);
+  }
   embed.addField('Denied By', message.member.user.tag, true);
   embed.setFooter(client.user.username, client.user.avatarURL);
   embed.setTimestamp();
 
   thisChannel.send(embed);
-  message.delete()
+  if (args[1]) {
+    try {
+      const resolvedUser1 = (args[1] !== undefined) ? message.guild.members.get(args[1].match(/[0-9]/g).join('')) : null;
+      const thisOwner = resolvedUser1 ? message.guild.members.get(resolvedUser1.id) : null;
+      thisOwner.send(embed);
+    } catch (err) {
+      thisChannel.send(`I tried to send the embed to the owner as well, but it failed with this error: ${err}`);
+    }
+  }
+  message.delete();
 
   if (client.approved.get(thisUser)) {
     client.approved.delete(thisUser);
@@ -50,5 +64,5 @@ exports.help = {
   name: 'deny',
   category: 'System',
   description: 'Denies a client user.',
-  usage: 'deny'
+  usage: 'deny [bot] [owner] [reason]'
 };
