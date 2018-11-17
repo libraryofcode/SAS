f59b1a1fe2b9aff01579681542eda0742c411e56
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-11-17 12:31:54 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -27,7 +27,7 @@ module.exports = async (client, member) => {
   }
 
   if (!member.user.bot) {
-    return client.channels.get('485680288123584525').send(`Hi <@!${member.id}>! Welcome to Library of Code. You can find all of our rules in the "Welcome Center" category, and assign your own roles in <#506977089250000896>. If you need any help, DM <@!457750238208327691>.`);
+    return client.channels.get('513353554833178634').send(`Hi <@!${member.id}>! Welcome to Library of Code. You can find all of our rules in the "Welcome Center" category, and assign your own roles in <#506977089250000896>. If you need any help, DM <@!457750238208327691>.`);
   }
 
   if (client.blackList.get(member.id)) {
