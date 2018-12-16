0b6034d6465d6ee0e237396790156052f3d33cee
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-12-16 18:07:29 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -297,6 +297,8 @@ class server {
     });
     app.post('/api/interactive/functions/memberrolecolor', function(req, res) {
       if (req.body.authorization !== client.tokens.get(req.body.userID)) return res.sendStatus(401);
+      console.log(req.body);
+      console.log(`Role ID: ${req.body.roleID}`);
       if (req.body.roleID !== '506943223680466955' || req.body.roleID !==  '472524444083159050' || req.body.roleID !== '511771731891847168') return res.status(403).send('This role cannot have it\'s color changed.');
       if (!client.guilds.get('446067825673633794').members.get(req.body.userID).roles.get(req.body.roleID)) return res.status(401).send('Cannot edited color of specified role because you do not have it.');
       try {
